05777550fe2addd7ead2cd63d52b1f88a88657af
Chris Peck <onthelan@gmail.com>
2021-04-19 21:52:24 -0400

    Initial commit

---
+++ Bank/Classes/BankAccount.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class BankAccount
+    {
+        private static int AccountNumberSeed = 1234567890;
+
+        public string AccountNumber { get; }
+        public string Owner { get; set; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.AccountNumber = AccountNumberSeed.ToString();
+            AccountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount <= 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Classes;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("<name>", 1000);
+            account.MakeWithdrawl(500, DateTime.Now, "Rent payment");
+            Console.WriteLine($"Account {account.AccountNumber} was created for {account.Owner} with {account.Balance} initial balance");
+        }
+    }
+}
